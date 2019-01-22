37249d994a0d4c6850ff7929de1b17a76dc7b5f3
GuillermoDeAnda <46821652+GuillermoDeAnda@users.noreply.github.com>
2019-01-22 11:04:24 -0600

    Guillermo De Anda Casas, Grupo 03

---
+++ Velocidad.py
@@ -0,0 +1,9 @@
+#Autor: Guillermo De Anda Casas
+#Calcula la velocidad promedio, dado el tiempo y la distancia
+
+d = int(input("Teclea la distancia de tu viaje en kilómetros enteros: "))
+t = int(input("Teclea el tiempo de tu viaje en horas enteras: "))
+
+v = d/t
+
+print("Velocidad promedio:",v, "km/h")
